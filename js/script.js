3562091c30f80ea24f89228f93c96c9d658228c2
IrRo <92632386+talgathia@users.noreply.github.com>
2023-10-23 14:55:40 +0200

    js - added functionality

--- js/script.js
+++ js/script.js
@@ -1,9 +1,35 @@
 let taskTable = document.querySelector('.task-table');
 let inputField = document.querySelector('.task-input');
+let tableBody = document.querySelector('.task-table-body');
 
-inputField.addEventListener('keypress', (e) => {
-    if(e.key === 'Enter') {
-        console.log('Hallo');
-    }
-});
 
+
+
+const addInputToTable = () => {
+    inputField.addEventListener('keypress', (e) => {
+        if(e.key === 'Enter') {
+            let input = inputField.value;
+            inputField.value = '';
+            // erstelle Tabellenzeile
+            let newRow = createTableRow();
+            addCellToTableRow(newRow, input);
+        }
+    });
+}
+
+
+
+addInputToTable();
+
+const createTableRow = () => {
+    let newTableRow = tableBody.insertRow(0);
+    return newTableRow;
+}
+
+const addCellToTableRow = (tableRow, input) => {
+    let taskCell = tableRow.insertCell();
+    let editCell = tableRow.insertCell();
+    let deleteCell = tableRow.insertCell();
+    let newText = document.createTextNode(input);
+    taskCell.appendChild(newText);
+}
