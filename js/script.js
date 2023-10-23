fa9351dfe30c415e37b35744215e5ca5db7b103e
IrRo <92632386+talgathia@users.noreply.github.com>
2023-10-23 15:45:21 +0200

    js - added icon-making

--- js/script.js
+++ js/script.js
@@ -2,15 +2,11 @@ let taskTable = document.querySelector('.task-table');
 let inputField = document.querySelector('.task-input');
 let tableBody = document.querySelector('.task-table-body');
 
-
-
-
 const addInputToTable = () => {
     inputField.addEventListener('keypress', (e) => {
         if(e.key === 'Enter') {
             let input = inputField.value;
             inputField.value = '';
-            // erstelle Tabellenzeile
             let newRow = createTableRow();
             addCellToTableRow(newRow, input);
         }
@@ -32,4 +28,14 @@ const addCellToTableRow = (tableRow, input) => {
     let deleteCell = tableRow.insertCell();
     let newText = document.createTextNode(input);
     taskCell.appendChild(newText);
+    
+    let deleteIcon = document.createElement('img');
+    deleteIcon.src = '../img/delete.png';
+    deleteIcon.classList.add('delete');
+    deleteCell.appendChild(deleteIcon);
+    
+    let editIcon = document.createElement('img');
+    editIcon.src = '../img/edit.png';
+    editIcon.classList.add('edit');
+    editCell.appendChild(editIcon);
 }
